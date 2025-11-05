5c27c65aeeabe254b5b3d8c1ba021edc404f3b00
tshavit2011-bit <tshavit2011@gmail.com>
2025-11-05 13:03:21 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -12,8 +12,9 @@ def find_mean(num1, num2, num3):
     mean = (num1 + num2 + num3) / 3
     return mean
 
-
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    std = ((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) ** 0.5
+    # חישוב סטיית תקן נכונה
+    variance = ((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3
+    std = variance ** 0.5
     return mean, std
